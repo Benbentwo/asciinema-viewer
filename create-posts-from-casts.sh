b94ce2d31c2294f8c8dda99dd02e442baafd0027
Benjamin Smith <ben.smith.developer@gmail.com>
2020-07-09 14:41:24 -0400

    Updating Readme, and create script

--- create-posts-from-casts.sh
+++ create-posts-from-casts.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
 dir='_posts'
 
+formatDate() {
+    unix_timestamp=$1
+    os=$(uname)
+    if [[ ${os} == "Darwin" ]]; then
+        formatteddate=$(date -ur ${unix_timestamp} +%Y-%m-%d)
+    elif [[ ${os} == "Linux" ]]; then
+        formatteddate=$(date -d @${unix_timestamp} +%Y-%m-%d)
+    fi
+    echo "Formatted Date: $formatteddate"
+    export RET=${formatteddate}
+}
+
+
 mkdir -p ${dir}
 baseurl=$(cat _config.yml | grep baseurl | head -n 1 | awk -F ':' '{print $2}'| tr -d '/' | tr -d ' ')
 for file in casts/*; do
     json=$(head -n 1 $file)
+    timestamp=$(echo ${json} | jq -r '.timestamp')
+    formattedDate=""
+    if [[ ${timestamp} ]]; then
+        formatDate ${timestamp}
+        formattedDate="${RET}"
+        unset RET
+    fi
     filename=$(basename $file  | awk -F '.' '{print $1}')
     cast_file=$(basename $file)
     raw_file_name=${filename}
-    curr_date=$(date +%Y-%m-%d)
-    filename=${curr_date}-${filename}.md
-#    TODO allow updating of files?
+#    curr_date=$(date +%Y-%m-%d)
+    filename=${formattedDate}-${filename}.md
     if [[ ! -f ${dir}/${filename} ]]; then
         echo "No file found... making one ${dir}/${filename}"
         echo "---" > ${dir}/${filename}
         echo 'title: "'${raw_file_name}'"' >> ${dir}/${filename}
 
-        timestamp=$(echo ${json} | jq -r '.timestamp')
-        if [[ ${timestamp} ]]; then
-            echo 'last_modified_at: "'${timestamp}'"' >> ${dir}/${filename} #TODO date formatting
+        if [[ $formattedDate ]]; then
+            echo 'last_modified_at: "'${unix_timestamp}'"' >> ${dir}/${filename} #TODO date formatting
         fi
 
         echo "source: "/${baseurl}/casts/${cast_file} >> ${dir}/${filename}
